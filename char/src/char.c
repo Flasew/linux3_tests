6a193669060285b13e917b60130b54d85eecb668
Alex Zahn <avzahn06@gmail.com>
2016-12-28 15:09:08 -0800

    .

--- char/src/char.c
+++ char/src/char.c
@@ -4,12 +4,18 @@
 
 #include <linux/module.h>
 
+#include <linux/fs.h>
+#include <linux/kdev_t.h>
+
 MODULE_LICENSE("Dual BSD/GPL");
 
 static char* target_dev;
 static int irq = 0;
 static int irqflags = 0x0080;
 static int major = 0;
+
+static int minor = 0;
+static dev_t dev;
 
 module_param(irq, int, 0644);
 module_param(irqflags, int, 0644);
@@ -19,12 +25,21 @@ module_param(target_dev, charp, 0644);
 
 static int __init char_init(void) {
 
+	dev = MKDEV(major,minor);
+
+	if (!register_chrdev_region(dev,1,"acb")){
+		printk(KERN_ALERT "[char] could not register character device");
+	}
+
+
 	printk(KERN_ALERT "[char] irq = %d, target=%s\n", irq, target_dev);
 
 	return 0;
 }
 
 static void __exit char_exit(void) {
+
+	unregister_chrdev_region(dev,1);
 
 	printk(KERN_ALERT "[char] unregistered\n");
 }
