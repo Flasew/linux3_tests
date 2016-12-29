75a2cd5851db4f0c5bc53ab30101f30519eec4dc
Alex Zahn <avzahn06@gmail.com>
2016-12-28 16:51:52 -0800

    .

--- char/launch.py
+++ char/launch.py
@@ -13,18 +13,18 @@ def get_majors():
 def unused_majors():
 	unused = []
 	majors = get_majors()
-	for i in range(max_major):
+	for i in range(1,max_major):
 		if not (i in majors):
 			unused.append(i)
 	return unused
 			
-def launch(m,irq,irqflags,target_dev):
+def launch(m,irq,irqflags,target_dev,name):
 	
 	m = int(m)
 	irq = int(irq)
 	irqflags=int(irqflags)
 
-	cmd = 'insmod char.ko major=%i irq=%i irqflags=%i target_dev=%s' % (m,irq,irqflags,target_dev);
+	cmd = 'insmod char.ko major=%i irq=%i irqflags=%i target_dev=%s name=%s' % (m,irq,irqflags,target_dev,name);
 	print cmd
 	subprocess.call(cmd,shell=True)
 
@@ -34,9 +34,10 @@ if __name__ == '__main__':
 	parser.add_option('--irq',dest='irq', default='0')
 	parser.add_option('--irqflags',dest='irqflags', default=0x0080)
 	parser.add_option('--dev',dest='target_dev', default='/dev/null')
+	parser.add_option('--name',dest='name',default='acb')
 	
 	o,r = parser.parse_args()
 	
 	m = unused_majors()[0]
-	launch(m, o.irq, o.irqflags, o.target_dev)
+	launch(m, o.irq, o.irqflags, o.target_dev,o,name)
 	
--- char/src/char.c
+++ char/src/char.c
@@ -7,29 +7,37 @@
 #include <linux/fs.h>
 #include <linux/kdev_t.h>
 
+#include <linux/cdev.h> 
+
 MODULE_LICENSE("Dual BSD/GPL");
 
 static char* target_dev;
+static char* name;
 static int irq = 0;
 static int irqflags = 0x0080;
 static int major = 0;
 
 static int minor = 0;
 static dev_t dev;
+static struct cdev * cdev;
 
 module_param(irq, int, 0644);
 module_param(irqflags, int, 0644);
 module_param(major, int, 0644);
+module_param(name, charp, 0644);
 module_param(target_dev, charp, 0644);
-
 
 static int __init char_init(void) {
 
 	dev = MKDEV(major,minor);
+	cdev_init
 
-	if (!register_chrdev_region(dev,1,"acb")){
+	if (!register_chrdev_region(dev,1,name)){
 		printk(KERN_ALERT "[char] could not register character device");
 	}
+
+
+
 
 
 	printk(KERN_ALERT "[char] irq = %d, target=%s\n", irq, target_dev);
