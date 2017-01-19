7342e95868e6037e21072ddbaae7c78e23673916
a am <a@a.com>
2017-01-19 07:26:40 -0800

    .

--- sysfs/char.c
+++ sysfs/char.c
@@ -2,17 +2,20 @@
 
 MODULE_LICENSE("Dual BSD/GPL");
 
+
+atomic_t the_value;
+
 static int __init char_init(void) {
 	
 	
 	alloc_chrdev_region(&devno,0,1,"testchar");
 	
 	cdev_init(&cdev, &fops);
-	cdev_add(&cdev, params.devno,1);
+	cdev_add(&cdev, devno,1);
 	
 	sysfs_class = class_create(THIS_MODULE,"ctestchar");
 	sysfs_device = device_create(sysfs_class,NULL,devno,NULL,"testchar");
-	device_create_file(sysfs_device,&some_attr);
+	device_create_file(sysfs_device,&dev_attr_some_attr);
 	
 	
 	printk(KERN_ALERT "[char] registered\n");
@@ -40,7 +43,7 @@ static ssize_t driver_write(struct file *filep,
 	loff_t *offset){
 		
 	printk(KERN_ALERT "[char] write");
-	
+	return 0;
 
 }
 
@@ -52,19 +55,19 @@ static int driver_open(struct inode *inodep, struct file *filep) {
 	return 0;
 }
 
+static ssize_t some_attr_show(struct device * dev, struct device_attribute * attr, char * buf){
 
+	return snprintf((char*)buf,PAGE_SIZE,"%d\n",the_value.counter);
 
+	
+}
 
-
-
-
-
-
-
-
-static ssize_t some_attr_show(struct device * dev, struct device_attribute * attr, char * buf);
-
-static ssize_t some_attr_store(struct device * dev, struct device_attribute * attr, char * buf, size_t count);
+static ssize_t some_attr_store(struct device * dev, struct device_attribute * attr, const char * buf, size_t count){
+	int tmp;
+	sscanf(buf,"%d",&tmp);
+	atomic_set(&the_value,tmp);
+	return PAGE_SIZE;
+}
 
 
 
--- sysfs/char.h
+++ sysfs/char.h
@@ -20,9 +20,6 @@ struct device * sysfs_device;
 static int driver_open(struct inode *inodep, struct file *filep);
 static int driver_release(struct inode *inodep, struct file *filep);
 
-
-
-
 static ssize_t driver_write(struct file *filep,	const char *buffer,	size_t len,	loff_t *offset);
 
 static struct file_operations fops = {	
@@ -35,7 +32,8 @@ static struct file_operations fops = {
 
 static ssize_t some_attr_show(struct device * dev, struct device_attribute * attr, char * buf);
 
-static ssize_t some_attr_store(struct device * dev, struct device_attribute * attr, char * buf, size_t count);
+static ssize_t some_attr_store(struct device * dev, struct device_attribute * attr, const char * buf, size_t count);
 
 
-DEVICE_ATTR(some_attr, S_IRUSR | S_IWUSR, some_attr_show, some_attr_store);
+
+DEVICE_ATTR(some_attr, S_IRWXU | S_IRWXG , some_attr_show, some_attr_store);
